2c5c8e27de3992c6b1ea39b90f29bc8305977eb7
satyamsingh5512 <satyamsinghnew5512@gmail.com>
2026-04-04 17:41:56 +0530

    Fix: allow LAN IP in Next.js dev CORS (allowedDevOrigins)

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  allowedDevOrigins: [
+    'http://172.29.50.122:3000',
+    '172.29.50.122',
+  ],
 }
 
 module.exports = nextConfig
