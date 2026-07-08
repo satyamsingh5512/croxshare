ef075eac6bb39725236a70b0283a5c0c191896ad
satyamsingh5512 <satyamsinghnew5512@gmail.com>
2026-07-08 14:11:29 +0530

    Update/Add/Remove client/src/main.js

--- client/src/main.js
+++
@@ -1,7 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import { TransferProvider } from './context/TransferContext';
-import './styles.css';
-ReactDOM.createRoot(document.getElementById('root')).render(_jsx(React.StrictMode, { children: _jsx(TransferProvider, { children: _jsx(App, {}) }) }));
